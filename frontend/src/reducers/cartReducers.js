0db7875eb2f90921f4b9f30e9a39813cf0af9a04
Pablets <godoypablom@gmail.com>
2021-01-18 11:18:01 -0300

    adding changes

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,23 +1,30 @@
-import { CART_ADD_ITEM } from '../constants/cardConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cardConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
         case CART_ADD_ITEM:
-            const item = action.payload
+      const item = action.payload
 
-            const existItem = state.cartItems.find(x => x.product === item.product)
+      const existItem = state.cartItems.find((x) => x.product === item.product)
 
-            if (existItem) {
-                return {
-                    ...state,
-                    cartItems: state.cartItems.map(x => x.product === existItem.product ? item : x)
-                }
-            } else {
-                return {
-                    ...state,
-                    carItems: [...state.cartItems, item]
-                }
-            }
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+          ),
+        }
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        }
+      }
+      case CART_REMOVE_ITEM:
+        return {
+          ...state,
+          cartItems: state.cartItems.filter((x) => x.product !== action.payload)
+        }
         default:
             return state
     }
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -84,12 +84,18 @@ const ProductScreen = ({ history, match }) => {
                                                 <Row>
                                                     <Col>Qty</Col>
                                                     <Col>
-                                                        <Form.Control as='select' value={qty} onChange={(e) => setQty(e.target.value)}>
-                                                            {[...Array(product.countInStock).keys()].map(x => (
-                                                                <option key={x + 1} value={x + 1}>
-                                                                    {x + 1}
-                                                                </option>
-                                                            ))}
+                                                        <Form.Control
+                                                            as='select'
+                                                            value={qty}
+                                                            onChange={(e) => setQty(e.target.value)}
+                                                        >
+                                                            {[...Array(product.countInStock).keys()].map(
+                                                                (x) => (
+                                                                    <option key={x + 1} value={x + 1}>
+                                                                        {x + 1}
+                                                                    </option>
+                                                                )
+                                                            )}
                                                         </Form.Control>
                                                     </Col>
                                                 </Row>
