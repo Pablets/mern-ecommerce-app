3e3bb2b0c9c33b6bcb4a73ac17bbab822866376d
Pablets <godoypablom@gmail.com>
2021-01-21 11:02:21 -0300

    control point

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -8,19 +8,22 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/');
   },
   filename(req, file, cb) {
-    cb(null, `${file.fieldname}-${Date.now}${path.extname(file.originalname)}`);
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
   },
 });
 
 function checkFileType(file, cb) {
   const filetypes = /jpg|jpeg|png/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase);
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
 
   if (extname && mimetype) {
     return cb(null, true);
   } else {
-    cb('Images only');
+    cb('Images only!');
   }
 }
 
